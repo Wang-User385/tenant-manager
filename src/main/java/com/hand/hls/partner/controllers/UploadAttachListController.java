c248dce4fe498e763913a65d26888ab1ec996cd2
黎时庆 <shiqing.li@hand-china.com>
2024-07-18 21:00:56 +0800

    获取文件上传URL接口

--- src/main/java/com/hand/hls/partner/controllers/UploadAttachListController.java
+++ src/main/java/com/hand/hls/partner/controllers/UploadAttachListController.java
@@ -1,17 +1,15 @@
 package com.hand.hls.partner.controllers;
 
+import com.alibaba.fastjson.JSONObject;
 import com.hand.hap.core.IRequest;
-import com.hand.hls.partner.dto.UploadAttachList;
 import org.springframework.stereotype.Controller;
 import com.hand.hap.system.controllers.BaseController;
-import com.hand.hap.system.dto.ResponseData;
 import com.hand.hls.partner.service.IUploadAttachListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -26,9 +24,19 @@ import org.springframework.web.bind.annotation.*;
                 value = {"/di/getUploadUrl"},
                 method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public ResponseData getUploadUrl(@RequestBody @Valid UploadAttachList uploadAttachList, HttpServletRequest request) {
+    public JSONObject getUploadUrl(@RequestBody JSONObject jsonObject, HttpServletRequest request) throws Exception {
         IRequest iRequest = createRequestContext(request);
-        return service.getUploadUrl(uploadAttachList,request,iRequest);
+        return service.getUploadUrl(jsonObject,request,iRequest);
+    }
+
+    @RequestMapping(
+            value = {"/di/upload"},
+            method = {RequestMethod.PUT})
+    @ResponseBody
+    public JSONObject upload(@RequestBody JSONObject jsonObject,@RequestParam("fileId")String fileId, HttpServletRequest request) throws Exception {
+        return null;
+//        IRequest iRequest = createRequestContext(request);
+//        return service.getUploadUrl(jsonObject,request,iRequest);
     }
 
 }
--- src/main/java/com/hand/hls/partner/service/impl/UploadAttachListServiceImpl.java
+++ src/main/java/com/hand/hls/partner/service/impl/UploadAttachListServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.hand.hap.core.IRequest;
 import com.hand.hap.system.dto.ResponseData;
 import com.hand.hap.system.service.impl.BaseServiceImpl;
+import com.hand.hls.partner.util.RsaAesUtils;
 import com.hand.hls.web.logs.dto.HlsWsRequests;
 import com.hand.hls.web.logs.service.IHlsWsRequestsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,7 @@ import com.hand.hls.partner.service.IUploadAttachListService;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -25,27 +24,38 @@ public class UploadAttachListServiceImpl extends BaseServiceImpl<UploadAttachLis
     private IHlsWsRequestsService logService;
 
     @Override
-    public ResponseData getUploadUrl(UploadAttachList uploadAttachList, HttpServletRequest request,IRequest iRequest){
+    public JSONObject getUploadUrl(JSONObject jsonObject, HttpServletRequest request,IRequest iRequest) throws Exception {
+        //step1 存储加密请求报文日志
         HlsWsRequests hlsWsRequests = new HlsWsRequests();
         hlsWsRequests.setRequestWsdlUrl(request.getRequestURI());
         hlsWsRequests.setFunctionName("GT-YL-F001获取文件上传URL接口");
-        hlsWsRequests.setRequestJson(JSONObject.toJSONString(uploadAttachList));
+        hlsWsRequests.setRequestJsonEncrypt(JSONObject.toJSONString(jsonObject));
         hlsWsRequests = logService.interfaceSave(hlsWsRequests,iRequest);
-
-        String fileId = "abc";
-        String uploadUrl = "http://2121";
+        //step2 解密请求报文，存储解密请求报文日志
+        String decryptedStr = RsaAesUtils.decryptedData(jsonObject);
+        hlsWsRequests.setRequestJson(decryptedStr);
+        hlsWsRequests = logService.interfaceSave(hlsWsRequests,iRequest);
+        //step3 请求报文转dto、业务逻辑处理
+        UploadAttachList uploadAttachList = JSONObject.parseObject(decryptedStr, UploadAttachList.class);
+        String fileId = UUID.randomUUID().toString();
+        String uploadUrl = "/r/api/di/upload?fileId=" + fileId;
         uploadAttachList.setFileId(fileId);
         uploadAttachList.setUploadUrl(uploadUrl);
         this.insertSelective(iRequest,uploadAttachList);
+        //step4 构造返回报文
+        JSONObject resJson = new JSONObject();
+        resJson.put("fileId",fileId);
+        resJson.put("uploadUrl",uploadUrl);
+        String resStr = JSONObject.toJSONString(resJson);
+        //step5 存储返回报文日志
+        hlsWsRequests.setResponseJson(resStr);
+        hlsWsRequests = logService.interfaceSave(hlsWsRequests,iRequest);
+        //step6 加密返回报文，存储加密返回报文日志
+        JSONObject encryptedResJson = RsaAesUtils.encryptedData(resStr);
+        hlsWsRequests.setResponseJsonEncrypt(JSONObject.toJSONString(encryptedResJson));
+        hlsWsRequests.setReturnStatus("S");
+        hlsWsRequests = logService.interfaceSave(hlsWsRequests,iRequest);
 
-        List<UploadAttachList> list = new ArrayList<>();
-        list.add(uploadAttachList);
-        ResponseData responseData = new ResponseData(list);
-
-        hlsWsRequests.setReturnStatus("200");
-        hlsWsRequests.setResponseJson(JSONObject.toJSONString(responseData));
-        logService.interfaceSave(hlsWsRequests,iRequest);
-
-        return responseData;
+        return encryptedResJson;
     }
 }
