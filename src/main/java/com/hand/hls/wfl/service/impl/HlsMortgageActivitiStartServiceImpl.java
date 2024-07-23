7ac4d9ff516dcfdcf3a4b929e60f71b5d6a0443d
ljz <JINGZHOU.LI@HAND-CHINA.COM>
2024-07-23 10:14:58 +0800

    车辆抵押开发，sql位置修正

---
+++ src/main/java/com/hand/hls/wfl/service/impl/HlsMortgageActivitiStartServiceImpl.java
@@ -0,0 +1,56 @@
+package com.hand.hls.wfl.service.impl;
+
+import com.hand.hap.activiti.service.IActivitiService;
+import com.hand.hap.core.IRequest;
+import com.hand.hls.gld.service.HlsCusConContractService;
+import com.hand.hls.utils.HlsCusConstant;
+import com.hand.hls.wfl.components.WflGetProcessInstanceComponents;
+import com.hand.hls.wfl.service.IActivitiCommonService;
+import org.activiti.rest.service.api.runtime.process.ProcessInstanceCreateRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ * description
+ * </p>
+ *
+ * @author JINGZHOU.LI@hand-china.com 2024/7/22 19:42
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class HlsMortgageActivitiStartServiceImpl implements IActivitiCommonService {
+    private static final String workFlowType = "CAR_MORTGAGE";
+
+    @Autowired
+    private IActivitiService activitiService;
+
+    @Autowired
+    private WflGetProcessInstanceComponents wflGetProcessInstanceComponents;
+
+    @Autowired
+    private HlsCusConContractService hlsCusConContractService;
+
+
+    @Override
+    public String getWorkFlowType() {
+        return workFlowType;
+    }
+
+    @Override
+    public void process(IRequest iRequest, List list, Map params) {
+        ProcessInstanceCreateRequest processInstanceCreateRequest = wflGetProcessInstanceComponents.getProcessInstance(iRequest, params);
+        activitiService.startProcess(iRequest, processInstanceCreateRequest);
+    }
+
+
+    @Override
+    public void cancel(IRequest iRequest, Map params) {
+
+    }
+
+}
