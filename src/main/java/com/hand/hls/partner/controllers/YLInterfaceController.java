33e2a85b182da482ec111019846a13a6ef3f2d2e
黎时庆 <shiqing.li@hand-china.com>
2024-08-02 14:34:09 +0800

    token进来的iRequest没有allocation_id等数据导致工作流启动问题，手工写构造iRequest的方法。

--- src/main/java/com/hand/hls/partner/controllers/YLInterfaceController.java
+++ src/main/java/com/hand/hls/partner/controllers/YLInterfaceController.java
@@ -1,19 +1,34 @@
 package com.hand.hls.partner.controllers;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hand.hap.account.dto.User;
+import com.hand.hap.account.mapper.UserMapper;
 import com.hand.hap.core.IRequest;
 import com.hand.hap.core.impl.RequestHelper;
 import com.hand.hap.system.controllers.BaseController;
 import com.hand.hls.partner.service.YLInterfaceService;
 import com.hand.hls.partner.util.RsaAesUtils;
+import com.hand.hls.sys.dto.FndEmployee;
+import com.hand.hls.sys.dto.SysUserAllocation;
+import com.hand.hls.sys.mapper.FndEmployeeMapper;
+import com.hand.hls.sys.mapper.SysUserAllocationMapper;
 import com.hand.hls.web.logs.dto.HlsWsRequests;
 import com.hand.hls.web.logs.service.IHlsWsRequestsService;
 import hls.core.utils.exception.HlsCusException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.slf4j.MDC;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 @RequestMapping(value = {"/r/api"})
@@ -23,6 +38,22 @@ public class YLInterfaceController extends BaseController {
     private YLInterfaceService ylInterfaceService;
     @Autowired
     private IHlsWsRequestsService logService;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private SysUserAllocationMapper sysUserAllocationMapper;
+    @Autowired
+    private FndEmployeeMapper fndEmployeeMapper;
+
+    private HttpServletRequest fakeRequest = new MockHttpServletRequest();
+    private static String LOCAL = "zh_CN";
+    private final IRequest setMdc(IRequest iRequest) {
+        Map<String, String> mdcMap = MDC.getCopyOfContextMap();
+        if (mdcMap != null) {
+            mdcMap.forEach((k, v) -> iRequest.setAttribute(IRequest.MDC_PREFIX.concat(k), v));
+        }
+        return iRequest;
+    }
 
     @RequestMapping(
             value = {"/di/placeOrder"},
@@ -542,14 +573,13 @@ public class YLInterfaceController extends BaseController {
             method = {RequestMethod.GET, RequestMethod.POST}
     )
     @ResponseBody
-    public JSONObject businessApplication(@RequestBody JSONObject jsonObject, HttpServletRequest request) {
-        IRequest iRequest = createRequestContext(request);
-        RequestHelper.setCurrentRequest(iRequest);
+    public JSONObject businessApplication(@RequestBody JSONObject jsonObject, HttpServletRequest request) throws HlsCusException {
+        IRequest iRequest = this.createIRequest(request);
 
         HlsWsRequests hlsWsRequests = null;
         try {
             hlsWsRequests = this.insertLogs("GT-YL-B004-业务申请",jsonObject,request);
-        } catch (Exception e) {
+        }catch (Exception e) {
             e.printStackTrace();
             JSONObject resJson = new JSONObject();
             resJson.put("success",false);
@@ -642,6 +672,58 @@ public class YLInterfaceController extends BaseController {
         hlsWsRequests.setReturnStatus(returnStatus);
         hlsWsRequests = logService.interfaceSave(hlsWsRequests,RequestHelper.getCurrentRequest());
         return encryptedResJson;
+    }
+
+    private IRequest createIRequest(HttpServletRequest request) throws HlsCusException {
+        Long userId = createRequestContext(request).getUserId();
+        User sysUser = userMapper.selectByPrimaryKey(userId);
+        if (ObjectUtils.isEmpty(sysUser)) {
+            throw new HlsCusException("未找到用户（userId：" + userId + "）!");
+        }
+
+        SysUserAllocation sysUserAllocation = new SysUserAllocation();
+        sysUserAllocation.setUserId(sysUser.getUserId());
+        List<SysUserAllocation> sysUserAllocationList = sysUserAllocationMapper.select(sysUserAllocation);
+        if (sysUserAllocationList.size() <= 0) {
+            throw new HlsCusException("用户" + sysUser.getUserName() + "没有进行用户分配!");
+        }
+
+        FndEmployee fndEmployee = fndEmployeeMapper.selectEmployeeInfoByAllocationId(sysUserAllocationList.get(0).getAllocationId().toString());
+        if (ObjectUtils.isEmpty(fndEmployee)) {
+            throw new HlsCusException("未找到员工(AllocationId：" + sysUserAllocationList.get(0).getAllocationId() + ")!");
+        }
+
+        HttpSession session = fakeRequest.getSession();
+
+        session.setAttribute("userId", sysUser.getUserId());
+        session.setAttribute("roleId", sysUserAllocationList.get(0).getRoleId());
+        session.setAttribute("userName", sysUser.getUserName());
+        session.setAttribute("companyId", sysUserAllocationList.get(0).getCompanyId());
+        session.setAttribute("roleIds", null);
+        session.setAttribute("allocationId", sysUserAllocationList.get(0).getAllocationId());
+        session.setAttribute("employeeCode", fndEmployee.getEmployeeCode());
+
+        IRequest iRequest = RequestHelper.createServiceRequest(fakeRequest);
+        iRequest.setLocale(LOCAL);
+        MDC.put("userId", sysUser.getUserId().toString());
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        MDC.put("requestId", uuid);
+        MDC.put("sessionId", "");
+        iRequest = setMdc(iRequest);
+
+        iRequest.setAttribute("userId", sysUser.getUserId());
+        iRequest.setAttribute("roleId", sysUserAllocationList.get(0).getRoleId());
+        iRequest.setAttribute("userName", sysUser.getUserName());
+        iRequest.setAttribute("companyId", sysUserAllocationList.get(0).getCompanyId());
+        iRequest.setAttribute("roleIds", null);
+        iRequest.setAttribute("allocationId", sysUserAllocationList.get(0).getAllocationId());
+        iRequest.setAttribute("employeeCode", fndEmployee.getEmployeeCode());
+
+        RequestHelper.setCurrentRequest(iRequest);
+        ServletWebRequest servletWebRequest = new ServletWebRequest(fakeRequest);
+        RequestContextHolder.setRequestAttributes(servletWebRequest);
+
+        return iRequest;
     }
 
 }
