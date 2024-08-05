b2f0192b53a3528072f1819cb67f8a91843594e2
ljz <JINGZHOU.LI@HAND-CHINA.COM>
2024-08-05 15:01:46 +0800

    转付确认开发

---
+++ src/main/java/com/hand/hls/partner/controllers/YLCshTransferPaymentController.java
@@ -0,0 +1,37 @@
+package com.hand.hls.partner.controllers;
+import com.hand.hls.partner.dto.YLCshTransferPaymentDto;
+import com.hand.hls.partner.service.IYLCshTransferPaymentService;
+import org.springframework.stereotype.Controller;
+import com.hand.hap.system.controllers.BaseController;
+import com.hand.hap.core.IRequest;
+import com.hand.hap.system.dto.ResponseData;
+import com.alibaba.fastjson.JSONArray;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import com.hand.hap.core.impl.RequestHelper;
+import leaf.bean.LeafRequestData;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class YLCshTransferPaymentController extends BaseController {
+
+    @Resource
+    private IYLCshTransferPaymentService service;
+
+
+    @RequestMapping(value = "/yl/csh/update/transfer/status")
+    @ResponseBody
+    public ResponseData update(@ModelAttribute(LEAF_PARAM_NAME) LeafRequestData requestData, HttpServletRequest request) {
+        IRequest requestCtx = createRequestContext(request);
+        RequestHelper.setCurrentRequest(requestCtx);
+        JSONArray param = (JSONArray) requestData.get("parameter");
+        List<YLCshTransferPaymentDto> ylCshTransferPaymentDtoList = param.toJavaList(YLCshTransferPaymentDto.class);
+        ylCshTransferPaymentDtoList = service.updateTransferStatus(requestCtx,ylCshTransferPaymentDtoList);
+        return new ResponseData(service.batchUpdate(requestCtx,ylCshTransferPaymentDtoList));
+    }
+
+
+}
---
+++ src/main/java/com/hand/hls/partner/service/IYLCshTransferPaymentService.java
@@ -0,0 +1,14 @@
+package com.hand.hls.partner.service;
+
+import com.hand.hap.core.IRequest;
+import com.hand.hap.core.ProxySelf;
+import com.hand.hap.system.service.IBaseService;
+import com.hand.hls.partner.dto.YLCshTransferPaymentDto;
+
+import java.util.List;
+
+
+public interface IYLCshTransferPaymentService extends IBaseService<YLCshTransferPaymentDto>, ProxySelf<IYLCshTransferPaymentService> {
+
+    List<YLCshTransferPaymentDto> updateTransferStatus(IRequest requestCtx,List<YLCshTransferPaymentDto> list);
+}
