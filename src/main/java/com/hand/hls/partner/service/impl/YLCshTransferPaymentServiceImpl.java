85aee27fccd0db8e433fe370d1df6e015dbdcaf0
ljz <JINGZHOU.LI@HAND-CHINA.COM>
2024-08-06 10:30:42 +0800

    转付确认服务层开发

--- src/main/java/com/hand/hls/partner/service/impl/YLCshTransferPaymentServiceImpl.java
+++ src/main/java/com/hand/hls/partner/service/impl/YLCshTransferPaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hand.hls.partner.service.impl;
 import com.hand.hap.core.IRequest;
 import com.hand.hap.system.service.impl.BaseServiceImpl;
+import com.hand.hls.cont.dto.HlsCusConContractCashflow;
+import com.hand.hls.cont.mapper.HlsCusConContractCashflowMapper;
 import com.hand.hls.csh.dto.HlsCusCshTransaction;
 import com.hand.hls.csh.mapper.HlsCusCshTransactionMapper;
 import com.hand.hls.csh.service.HlsCusCshTransactionService;
@@ -8,10 +10,12 @@ import com.hand.hls.fnd.service.FndCodingRuleValuesService;
 import com.hand.hls.partner.dto.YLCshTransferPaymentDto;
 import com.hand.hls.partner.mapper.YLCshTransferPaymentDtoMapper;
 import com.hand.hls.partner.service.IYLCshTransferPaymentService;
+import com.hand.hls.utils.HlsCusMathUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -28,6 +32,9 @@ public class YLCshTransferPaymentServiceImpl extends BaseServiceImpl<YLCshTransf
 
     @Autowired
     private HlsCusCshTransactionMapper hlsCusCshTransactionMapper;
+
+    @Autowired
+    private HlsCusConContractCashflowMapper hlsCusConContractCashflowMapper;
 
     @Autowired
     private FndCodingRuleValuesService fndCodingRuleValuesService;
@@ -58,28 +65,36 @@ public class YLCshTransferPaymentServiceImpl extends BaseServiceImpl<YLCshTransf
     public List<YLCshTransferPaymentDto> updateAndVerification( IRequest requestCtx ,List<YLCshTransferPaymentDto> ylCshTransferPaymentDtoList) {
         ylCshTransferPaymentDtoList.forEach(ylCshTransferPaymentDto -> {
             //生产现金事务数据
-            HlsCusCshTransaction hlsCusCshTransaction = new HlsCusCshTransaction();
-            Map<String, String> params = new HashMap<>();
-            String transactionNum = fndCodingRuleValuesService.getCodeRuleValue(requestCtx, DOCUMENT_CATEGORY, DOCUMENT_TYPE, BUSINESS_TYPE, params);
-            hlsCusCshTransaction.setTransactionCategory(DOCUMENT_CATEGORY);
-            hlsCusCshTransaction.setTransactionType(DOCUMENT_TYPE);
-            hlsCusCshTransaction.setBusinessType(BUSINESS_TYPE);
-            hlsCusCshTransaction.setTransactionNum(transactionNum);
-            hlsCusCshTransaction.setTransactionDate(new Date());
-            hlsCusCshTransaction.setPenaltyCalcDate(hlsCusCshTransaction.getTransactionDate());
-            hlsCusCshTransaction.setCompanyId(requestCtx.getCompanyId());
-            hlsCusCshTransaction.setTransactionAmount(ylCshTransferPaymentDto.getRepayAmount());
-            hlsCusCshTransaction.setContractId(ylCshTransferPaymentDto.getContractId());
-            //设置核销
-//            hlsCusCshTransaction.setWriteOffFlag();
-//            hlsCusCshTransaction.setWriteOffAmount();
-            hlsCusCshTransaction.setReversedFlag("N");
-            hlsCusCshTransaction.setPostedFlag("N");
-            //系统字段
-            hlsCusCshTransaction.setCreationDate(new Date());
-            hlsCusCshTransaction.setCreatedBy(requestCtx.getUserId());
-            hlsCusCshTransaction.setLastUpdateDate(new Date());
-            hlsCusCshTransaction.setLastUpdatedBy(requestCtx.getUserId());
+            HlsCusCshTransaction hlsCusCshTransaction = getHlsCusCshTransaction(requestCtx, ylCshTransferPaymentDto);
+            //查出合同现金流数据
+            HlsCusConContractCashflow hlsCusConContractCashflow = new HlsCusConContractCashflow();
+            hlsCusConContractCashflow.setCashflowId(ylCshTransferPaymentDto.getCashflowId());
+            hlsCusConContractCashflow = hlsCusConContractCashflowMapper.selectByPrimaryKey(hlsCusConContractCashflow);
+            //应收金额
+            Double dueAmount = hlsCusConContractCashflow.getDueAmount();
+            //已收金额
+            Double receivedAmount = hlsCusConContractCashflow.getReceivedAmount();
+            //不存在应收小于已收 逾期和罚息是新的现金流
+            //已收加还款总金额
+            Double amount = HlsCusMathUtil.add(receivedAmount,ylCshTransferPaymentDto.getRepayAmount());
+            //应收  == 已收加还款
+            if (HlsCusMathUtil.compare(amount,dueAmount) == -1){
+                throw new RuntimeException("多还金额");
+            }
+            //设置核销字段
+            if (HlsCusMathUtil.compare(amount,dueAmount) == 0){
+                //部分核销
+                hlsCusCshTransaction.setWriteOffFlag("PARTIAL");
+                hlsCusConContractCashflow.setWriteOffFlag("PARTIAL");
+            }else {
+                //完全核销
+                hlsCusCshTransaction.setWriteOffFlag("FULL");
+                hlsCusConContractCashflow.setWriteOffFlag("FULL");
+            }
+            //设置核销金额
+            hlsCusCshTransaction.setWriteOffAmount(HlsCusMathUtil.add(hlsCusCshTransaction.getUnWriteOffAmount(),ylCshTransferPaymentDto.getRepayAmount()));
+            //设置还款后的已收金额
+            hlsCusConContractCashflow.setReceivedAmount(amount);
             hlsCusCshTransactionMapper.insertSelective(hlsCusCshTransaction);
 
 
@@ -90,4 +105,35 @@ public class YLCshTransferPaymentServiceImpl extends BaseServiceImpl<YLCshTransf
         });
         return ylCshTransferPaymentDtoList;
     }
+
+    private HlsCusCshTransaction getHlsCusCshTransaction(IRequest requestCtx, YLCshTransferPaymentDto ylCshTransferPaymentDto) {
+        HlsCusCshTransaction hlsCusCshTransaction = new HlsCusCshTransaction();
+        Map<String, String> params = new HashMap<>();
+        String transactionNum = fndCodingRuleValuesService.getCodeRuleValue(requestCtx, DOCUMENT_CATEGORY, DOCUMENT_TYPE, BUSINESS_TYPE, params);
+        hlsCusCshTransaction.setTransactionCategory(DOCUMENT_CATEGORY);
+        hlsCusCshTransaction.setTransactionType(DOCUMENT_TYPE);
+        hlsCusCshTransaction.setBusinessType(BUSINESS_TYPE);
+        hlsCusCshTransaction.setTransactionNum(transactionNum);
+        hlsCusCshTransaction.setTransactionDate(new Date());
+        hlsCusCshTransaction.setPenaltyCalcDate(hlsCusCshTransaction.getTransactionDate());
+        hlsCusCshTransaction.setCompanyId(requestCtx.getCompanyId());
+        hlsCusCshTransaction.setTransactionAmount(ylCshTransferPaymentDto.getRepayAmount());
+        hlsCusCshTransaction.setContractId(ylCshTransferPaymentDto.getContractId());
+        hlsCusCshTransaction.setReversedFlag("N");
+        hlsCusCshTransaction.setPostedFlag("N");
+        //系统字段
+        hlsCusCshTransaction.setCreationDate(new Date());
+        hlsCusCshTransaction.setCreatedBy(requestCtx.getUserId());
+        hlsCusCshTransaction.setLastUpdateDate(new Date());
+        hlsCusCshTransaction.setLastUpdatedBy(requestCtx.getUserId());
+        return hlsCusCshTransaction;
+    }
+
+    public static void main(String[] args) {
+        Double a = 2D;
+        Double b = 1D;
+        System.out.println(HlsCusMathUtil.compare("1", "1"));
+        System.out.println(HlsCusMathUtil.compare(a, b));
+    }
+
 }
