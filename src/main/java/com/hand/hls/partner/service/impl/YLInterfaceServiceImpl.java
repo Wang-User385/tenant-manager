679acda99bb80ffc6425f8ddb9a9465dc4f16ce5
dql <qianlong.deng@hand-china.com>
2024-08-19 15:21:38 +0800

    B005关单请求合同起租状态校验添加

--- src/main/java/com/hand/hls/partner/service/impl/YLInterfaceServiceImpl.java
+++ src/main/java/com/hand/hls/partner/service/impl/YLInterfaceServiceImpl.java
@@ -397,6 +397,11 @@ public class YLInterfaceServiceImpl implements YLInterfaceService {
                 throw new HlsCusException(returnJson.toJSONString());
             }
         }
+        if(hlsCusPrjProject.getOrderStatus().equals("INCEPT")){
+            returnJson.put("code","100101");
+            returnJson.put("message","合同已起租，不允许此操作");
+            throw new HlsCusException(returnJson.toJSONString());
+        }
         //关单后，如果蚂蚁链代扣是签约状态，则需调用取消签约
         if("ACTIVATED".equals(hlsCusPrjProject.getAlipayStatus())){
             iAlipayService.signCancel(hlsCusPrjProject.getProjectId());
