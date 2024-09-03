564702f355a4edda0159695cdca0a44c85ba5912
46997lipeng <lipeng.wang02@handchina.com>
2024-09-03 16:16:19 +0800

    授信审批主页面开发 【46997】

--- src/main/java/com/hand/hls/prj/mapper/HlsCusPrjProjectMapper.java
+++ src/main/java/com/hand/hls/prj/mapper/HlsCusPrjProjectMapper.java
@@ -148,7 +148,7 @@ public interface HlsCusPrjProjectMapper extends Mapper<HlsCusPrjProject> {
 
     //项目尽调首页查询 prjHomePageProjectInfoGridHome
     List<HlsCusPrjProject> prjHomePageProjectInfoGridHome(HlsCusPrjProject prjProject);
-    List<HlsCusPrjProject> queryAll(HlsCusPrjProject prjProject);
+    List<HlsCusPrjProject> queryProjectAll(HlsCusPrjProject prjProject);
     Long queryAssistUnitId(Long assistProjectManager);
 
     List<HlsCusPrjProject> queryAllApproveContract(HlsCusPrjProject hlsCusPrjProject);
